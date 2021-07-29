d3ed46d37b8999330184f6ef6d124633035166d7
vuuminhphuoc <vuuminhphuoc@gmail.com>
2021-07-29 09:27:18 +0700

    deletet

--- src/js/config.js
+++ src/js/config.js
@@ -1,5 +1,5 @@
 export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
 export const TIMEOUT_SEC = 10;
 export const RES_PER_PAGE = 6;
-export const KEY = 'a2709b2e-5388-4196-a508-46977c523b4a';
+export const KEY = '96cdea0d-262e-4ac5-b283-70b199f1c9df';
 export const MODAL_CLOSE_SEC = 2.5;
